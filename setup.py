b7695bdbd2001440af5be28c54e3e59df3686d6a
Filip Ginter <ginter@cs.utu.fi>
2014-08-20 17:27:10 +0300

    march=native compile for cython

--- setup.py
+++ setup.py
@@ -6,9 +6,11 @@ from Cython.Distutils import build_ext
 ext_modules = [Extension("perceptron_opt",
                      ["perceptron_opt.pyx", "MurmurHash3.cpp"],
                      language='c++',
+                     extra_compile_args = ["-march=native","-O6","-ffast-math"],
                      )]
 
 
 setup(name="perceptron_opt",
       cmdclass = {'build_ext': build_ext},
       ext_modules = ext_modules)
+
