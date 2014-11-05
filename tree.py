27924467826f7c2f1561b54068290a74cf276e6a
Juhani Luotolahti <mjluot@epsilon-it.utu.fi>
2014-11-05 17:41:34 +0200

    tree_feats

--- tree.py
+++ tree.py
@@ -2,12 +2,11 @@
 from collections import defaultdict,namedtuple
 import codecs
 import copy
-import itertools
 
-CoNLLFormat=namedtuple("CoNLLFormat",["ID","FORM","LEMMA","POS","FEAT","HEAD","DEPREL","EXTRA"])
+CoNLLFormat=namedtuple("CoNLLFormat",["ID","FORM","LEMMA","POS","FEAT","HEAD","DEPREL"])
 
 #Column lists for the various formats
-formats={"conll09":CoNLLFormat(0,1,2,4,6,8,10,12),"conll-u":CoNLLFormat(0,1,2,3,5,6,7,8)}
+formats={"conll09":CoNLLFormat(0,1,2,4,6,8,10)}
 
 
 
@@ -19,30 +18,22 @@ def read_conll(inp):
         f=inp
 
     sent=[]
-    comments=[]
     for line in f:
         line=line.strip()
-        if not line:
-            if sent:
-                yield sent, comments
+        if not line or line.startswith(u"#"): #Do not rely on empty lines in conll files, ignore comments
+            continue 
+        if line.startswith(u"1\t") and sent: #New sentence, and I have an old one to yield
+            yield sent
             sent=[]
-            comments=[]
-        elif line.startswith(u"#"):
-            if sent:
-                raise ValueError("Missing newline after sentence")
-            comments.append(line)
-            continue
-        else:
-            sent.append(line.split(u"\t"))
+        sent.append(line.split(u"\t"))
     else:
         if sent:
-            yield sent, comments
+            yield sent
 
     if isinstance(inp,basestring):
         f.close() #Close it if you opened it
 
-
-def fill_conll(sent,state,conll_format=u"conll-u"):
+def fill_conll(sent,state,conll_format=u"conll09"):
     form=formats[conll_format]
     for i in xrange(0,len(sent)):
         token=state.tree.tokens[i]
@@ -55,9 +46,8 @@ def fill_conll(sent,state,conll_format=u"conll-u"):
             sent[i][form.DEPREL]=u"ROOT"
         else:
             sent[i][form.DEPREL]=state.tree.dtypes[token]
-def write_conll(f,sent,comments=[]):
-    for comm in comments:
-        f.write(comm+u"\n")
+
+def write_conll(f,sent):
     for line in sent:
         f.write(u"\t".join(c for c in line)+u"\n")
     f.write(u"\n")
@@ -65,15 +55,10 @@ def write_conll(f,sent,comments=[]):
 class Tree(object):
 
     @classmethod
-    def new_from_conll(cls,conll,conll_format="conll-u",extra_tree=True): # TODO extra_tree
+    def new_from_conll(cls,conll,syn,conll_format="conll09"):
         t=cls()
-        form=formats[conll_format]
-        if conll[0][form.EXTRA]!=u"_" and extra_tree: # if not empty TODO
-            extra=Tree() # create empty tree
-        else:
-            extra=None
-        t.from_conll(conll,conll_format,extra)
-        return t,extra
+        t.from_conll(conll,syn,conll_format)
+        return t
 
     @classmethod
     def new_from_tree(cls,t):
@@ -106,28 +91,9 @@ class Tree(object):
         self.root=None #?
         self.projective_order=None 
         self.ready=False
-        self.semeval_root_idx=None
-
-    def BFS_queue(self,token):
-        result_dict={token:0} #itself at distance 0
-        self.BFS_order(token,0,result_dict) 
-        #result_dict should now have a distance for every reachable token
-        #dependency distance from token, linear distance from token, position in sentence, and the second token itself
-        queue=[(dis,abs(t.index-token.index),t.index,t) for t,dis in result_dict.iteritems()]
-        queue.sort()
-        return [item[-1] for item in queue] #pick just the token, nothing else
-
-    def BFS_order(self,token,distance,result_dict):
-        """
-        result_dict {token:distance}
-        """
-        for t in itertools.chain(self.childs[token],[self.govs.get(token,None)]):
-            if t is not None and t not in result_dict: #not visited yet (can happen on the way up in the tree) and not None (could come from .govs)
-                result_dict[t]=distance+1 #children and roots are at a distance one longer than the token itself
-                self.BFS_order(t,distance+1,result_dict)
 
     #Called from new_from_conll() classmethod
-    def from_conll(self,lines,conll_format="conll-u",extra=None):
+    def from_conll(self,lines,syn,conll_format="conll09"):    
         """ Reads conll format and transforms it to a tree instance. `conll_format` is a format name
             which will be looked up in the formats module-level dictionary"""
         form=formats[conll_format] #named tuple with the column indices
@@ -135,45 +101,24 @@ class Tree(object):
             line=lines[i]
             token=Token(i,line[form.FORM],pos=line[form.POS],feat=line[form.FEAT],lemma=line[form.LEMMA])
             self.tokens.append(token)
-            if extra is not None:
-                extra.tokens.append(token)
-            if line[form.DEPREL]==u"ROOT":
-                self.semeval_root_idx=i
-                self.tokens[-1].is_semeval_root=True
-            else:
-                self.tokens[-1].is_semeval_root=False
-        self.ready=False
-        if extra is not None: # create extra tree
-            for line in lines:
-                head,deprel=line[form.EXTRA].split(u":")
-                extra.dtypes[extra.tokens[int(line[form.ID])-1]]=deprel # fill dtype for token
-                gov=int(head)
-                if gov==0:
-                    extra.root=extra.tokens[int(line[form.ID])-1] # TODO: why I store this information?
-                    continue
-                gov=extra.tokens[gov-1]
-                dep=extra.tokens[int(line[form.ID])-1]
-                dependency=Dep(gov,dep,deprel)
-                extra.add_dep(dependency)
-            extra.ready=True
-            extra.ready_nodes=set(self.tokens) # all nodes are ready
 
-    def fill_syntax(self,sent,conll_format="conll-u"):
-        form=formats[conll_format] #named tuple with the column indices
-        for line in sent:
-            self.dtypes[self.tokens[int(line[form.ID])-1]]=line[form.DEPREL] # fill dtype for token
-            gov=int(line[form.HEAD])
-            if gov==0:
-                self.root=self.tokens[int(line[0])-1] # TODO: why I store this information?
-                continue
-            gov=self.tokens[gov-1]
-            dep=self.tokens[int(line[0])-1]
-            dType=line[form.DEPREL]
-            dependency=Dep(gov,dep,dType)
-            self.add_dep(dependency)
-        self.ready=True
-        self.ready_nodes=set(self.tokens) # all nodes are ready
         
+        if syn: # create dependencies
+            for line in lines:
+                self.dtypes[self.tokens[int(line[form.ID])-1]]=line[form.DEPREL] # fill dtype for token
+                gov=int(line[form.HEAD])
+                if gov==0:
+                    self.root=self.tokens[int(line[0])-1] # TODO: why I store this information?
+                    continue
+                gov=self.tokens[gov-1]
+                dep=self.tokens[int(line[0])-1]
+                dType=line[form.DEPREL]
+                dependency=Dep(gov,dep,dType)
+                self.add_dep(dependency)
+            self.ready=True
+            self.ready_nodes=set(self.tokens) # all nodes are ready
+        else: 
+            self.ready=False
 
     def add_dep(self,dependency):
         self.deps.append(dependency)
@@ -253,10 +198,74 @@ class Tree(object):
         elif (tok1.index>tok2.index) and (self.projective_order.index(tok1)<self.projective_order.index(tok2)): return True
         else: return False
 
-    def __str__(self):
-        return (u",".join(str(d) for d in self.deps)).encode(u"utf-8")
+    def get_token_tree_context(self, token, max_len=3, route=[]):
 
+        #If max_len less than 1 return what was given
+        if max_len < 1:
+            return [route]
 
+        #Get every dep in which current token is mentioned
+        where_to_go = []
+        for dep in tree.deps:
+            if start in dep.gov and dep not in route:
+                where_to_go.append((dep.dep, dep))
+            if start in dep.dep and dep not in route:
+                where_to_go.append((dep.gov, dep))        
+
+        #If max_len less than 2 return our findings
+        routes = []
+        if max_len == 1:
+            for start_token, dep in where_to_go:
+                routes.append(route + [dep])
+            return routes
+
+        #Otherwise, we'll get more routes
+        for start_token, dep in where_to_go:
+            routes.extend(get_token_tree_context(start_token, tree, max_len=max_len - 1, route + [dep]))
+
+        return routes
+
+    def get_route_in_tree(self, start, target, route=[]):
+
+        #Get every dep in which current token is mentioned
+        where_to_go = []
+        for dep in tree.deps:
+            if start in dep.gov and dep not in route:
+                if dep.dep = target:
+                    #Found it!
+                    route.append(dep)
+                    return route
+                where_to_go.append((dep.dep, dep))
+            if start in dep.dep and dep not in route:
+                if dep.gov = target:
+                    #Found it!
+                    route.append(dep)
+                    return route
+                where_to_go.append((dep.gov, dep))
+
+        if len(where_to_go) == 0:
+            return []
+
+        for start_token, dep in where_to_go:
+            result = get_route_in_tree(start_token, target, tree, route=route + [dep])
+            if result != []:
+                return result
+        return []
+
+    def create_token_routes(self):
+        #This is for emergency use only!
+        self.routes = {}
+        for t in self.tokens:
+            if t not in self.routes.keys():
+                self.routes[dep.gov] = {}
+            for tt in self.tokens:
+                self.route[t][tt] = get_route_in_tree(t, tt)
+
+    def create_context_routes(self):
+        #This is quite likely not very optimal
+        self.context = {}
+        for t in self.tokens:
+            self.context[t] = get_token_tree_context(t, max_len=3)
 
 class Token(object):
 
@@ -266,7 +275,6 @@ class Token(object):
         self.pos=pos
         self.feat=feat
         self.lemma=lemma
-        self.is_semeval_root=False
 
     def __str__(self):
         return self.text.encode(u"utf-8")
