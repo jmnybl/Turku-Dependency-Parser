53889e66e33b7b8d401a2c5e8a5468f4d5524255
Filip Ginter <ginter@cs.utu.fi>
2015-01-09 21:20:24 +0200

    Half way there with better taito scripts

--- t_and_t_lang.sh
+++ t_and_t_lang.sh
@@ -4,30 +4,35 @@ BEAM=$3
 
 mkdir -p out
 
-python train_parallel.py -p $CPUs --cpu-aff --beam $BEAM -o model.$LANG.taito.b$BEAM semeval_data/train.$LANG.conllu &
-PARSER_PID=$!
+python train_parallel.py -p $CPUs --cpu-aff --beam $BEAM -o model.$LANG.taito.b$BEAM semeval_data/train.$LANG.conllu 
 
-while [[ 1 ]]
-do
-    sleep 10
-    #Check for new models
-    for M in model.$LANG.taito.b$BEAM.i*
-    do
-	if [[ ! -e out/devel.$M.st_format ]]
-	then
-	    #New model! Parse it!
-	    #..stop training
-	    kill --signal SIGSTOP $PARSER_PID
-	    python parse_parallel -p $CPUs --cpu-aff $M semeval_data/devel.$L.conllu > out/devel.$M.conllu
-	    python seval2tree.py -r < out/devel.$M.conllu > out/devel.$M.stformat
-	    kill --signal SIGCONT $PARSER_PID
-	fi
-    done
-    #Has the parser finished?
-    ps -p $PARSER_PID > /dev/null
-    if [[ $? ]]
-    then
-	exit
-    fi
-done
+exit
+
+
+# PARSER_PID=$!
+
+# while [[ 1 ]]
+# do
+#     #Check for new models
+#     for M in model.$LANG.taito.b$BEAM.i*
+#     do
+# 	if [[ ! -f out/devel.$M.st_format && -f $M]]
+# 	then
+# 	    echo "New model $M"
+# 	    #New model! Parse it!
+# 	    #..stop training
+# 	    kill -s SIGSTOP $PARSER_PID
+# 	    python parse_parallel -p $CPUs --cpu-aff $M semeval_data/devel.$L.conllu > out/devel.$M.conllu
+# 	    python seval2tree.py -r < out/devel.$M.conllu > out/devel.$M.stformat
+# 	    kill - SIGCONT $PARSER_PID
+# 	fi
+#     done
+#     #Has the parser finished?
+#     ps -p $PARSER_PID > /dev/null
+#     if [[ $? ]]
+#     then
+# 	exit
+#     fi
+#     sleep 10 #...try again in 10s
+# done
 
--- train-taito.sh
+++ train-taito.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-CPUs=9
 
-echo "sbatch -t 24:0:0 -N1 --ntasks-per-node=1 -c$CPUs -e b10_lin.err -o b10_lin.out --mem-per-cpu=800 --wrap 'python train_parallel.py -p $CPUs --cpu-aff --beam 10 -o model.dm.taito.b10 semeval_data/train_dm_trees.conll'"
+#Call with no args for all languages or
+#./train-taito.sh cs.psd en.psd
+#for example
 
+
+CPUs=11
+BEAM=10
+
+if [[ ! $1 ]]
+then
+    LANGS="cs.psd en.dm en.pas en.psd"
+else
+    LANGS="$*"
+fi
+
+for L in $LANGS
+do
+   echo "sbatch -t 24:0:0 -N1 --ntasks-per-node=1 -J $L -c$CPUs -e log/$L.err -o log/$L.out --mem-per-cpu=800 --wrap './t_and_t_lang.sh $L $CPUs $BEAM'"
+done
